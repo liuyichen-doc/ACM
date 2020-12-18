9dacb4fab927be5bdb294c2a50d8ac11ada970ea
13952 <1395264153@qq.com>
2020-12-18 19:55:53 +0800

    init repo

---
+++ src/main/java/online/chinascliuyitao/acm/AcmApplication.java
@@ -0,0 +1,13 @@
+package online.chinascliuyitao.acm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmApplication.class, args);
+    }
+
+}
